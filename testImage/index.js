cdf62c8508d7072aa23762d1e5049b9272c089fe
feng <908087098@qq.com>
2019-12-20 15:49:54 +0800

    1

--- testImage/index.js
+++ testImage/index.js
@@ -158,9 +158,69 @@ function loadTexture(gl, n, texture, u_Sampler, image)
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); // Draw the rectangle
 }
 
-function loadToTexture(url)
+function loadImage(url, callback)
 {
+  var image = new Image();  // Create the image object
+  if (!image)
+  {
+    console.log('Failed to create the image object');
+    return false;
+  }
+  // Register the event handler to be called on loading an image
+  image.onload = function () { callback(image); };
+  // Tell the browser to load an image
+  image.src = '../resources/sky.jpg';
+}
 
+function loadImages(urls, callback, images)
+{
+  images = images || [];
+  if (urls.length == 0)
+  {
+    callback(images);
+    return;
+  }
+  var url = urls.pop();
+  loadImage(url, (image) =>
+  {
+    images.push(image);
+    loadImages(urls, callback, images);
+  });
+}
 
+function loadTexture1(gl, image)
+{
+  var texture = gl.createTexture();   // Create a texture object
+  if (!texture)
+  {
+    console.log('Failed to create the texture object');
+    return false;
+  }
+  // Bind the texture object to the target
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  //
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
+  // Set the texture parameters
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  // Set the texture image
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  return texture;
+}
 
+function draw(gl, n, texture, u_Sampler)
+{
+  // Enable texture unit0
+  gl.activeTexture(gl.TEXTURE0);
+  // Bind the texture object to the target
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Set the texture parameters
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+  // Set the texture unit 0 to the sampler
+  gl.uniform1i(u_Sampler, 0);
+
+  gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
+
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); // Draw the rectangle
 }
