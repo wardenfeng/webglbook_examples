69ca82b420de6081826e11166d93ea3c2ff305bc
feng <908087098@qq.com>
2019-12-20 16:08:15 +0800

    1

--- testImage/index.js
+++ testImage/index.js
@@ -20,13 +20,17 @@ var FSHADER_SOURCE =
   '  gl_FragColor = texture2D(u_Sampler, v_TexCoord);\n' +
   '}\n';
 
+var gl;
+var u_Sampler;
+var n;
+
 function main()
 {
   // Retrieve <canvas> element
   var canvas = document.getElementById('webgl');
 
   // Get the rendering context for WebGL
-  var gl = getWebGLContext(canvas);
+  gl = getWebGLContext(canvas);
   if (!gl)
   {
     console.log('Failed to get the rendering context for WebGL');
@@ -41,7 +45,7 @@ function main()
   }
 
   // Set the vertex information
-  var n = initVertexBuffers(gl);
+  n = initVertexBuffers(gl);
   if (n < 0)
   {
     console.log('Failed to set the vertex information');
@@ -49,7 +53,7 @@ function main()
   }
 
   // Get the storage location of u_Sampler
-  var u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
+  u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
   if (!u_Sampler)
   {
     console.log('Failed to get the storage location of u_Sampler');
@@ -59,29 +63,17 @@ function main()
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-  loadImage('../resources/sky.jpg', (image) =>
-  {
-    var texture = loadTexture1(gl, image);
-
-    draw(gl, n, texture, u_Sampler);
-  });
-
-  // // Set texture
-  // if (!initTextures(gl, n))
-  // {
-  //   console.log('Failed to intialize the texture.');
-  //   return;
-  // }
+  requestAnimationFrame(update)
 }
 
 function initVertexBuffers(gl)
 {
   var verticesTexCoords = new Float32Array([
     // Vertex coordinates, texture coordinate
-    -0.5, 0.5, 0.0, 1.0,
-    -0.5, -0.5, 0.0, 0.0,
-    0.5, 0.5, 1.0, 1.0,
-    0.5, -0.5, 1.0, 0.0,
+    -1, 1, 0.0, 1.0,
+    -1, -1, 0.0, 0.0,
+    1, 1, 1.0, 1.0,
+    1, -1, 1.0, 0.0,
   ]);
   var n = 4; // The number of vertices
 
@@ -122,57 +114,6 @@ function initVertexBuffers(gl)
   return n;
 }
 
-function initTextures(gl, n)
-{
-  var texture = gl.createTexture();   // Create a texture object
-  if (!texture)
-  {
-    console.log('Failed to create the texture object');
-    return false;
-  }
-
-  // Get the storage location of u_Sampler
-  var u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
-  if (!u_Sampler)
-  {
-    console.log('Failed to get the storage location of u_Sampler');
-    return false;
-  }
-  var image = new Image();  // Create the image object
-  if (!image)
-  {
-    console.log('Failed to create the image object');
-    return false;
-  }
-  // Register the event handler to be called on loading an image
-  image.onload = function () { loadTexture(gl, n, texture, u_Sampler, image); };
-  // Tell the browser to load an image
-  image.src = '../resources/sky.jpg';
-
-  return true;
-}
-
-function loadTexture(gl, n, texture, u_Sampler, image)
-{
-  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
-  // Enable texture unit0
-  gl.activeTexture(gl.TEXTURE0);
-  // Bind the texture object to the target
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-
-  // Set the texture parameters
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-  // Set the texture image
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
-
-  // Set the texture unit 0 to the sampler
-  gl.uniform1i(u_Sampler, 0);
-
-  gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
-
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); // Draw the rectangle
-}
-
 function loadImage(url, callback)
 {
   var image = new Image();  // Create the image object
@@ -203,7 +144,7 @@ function loadImages(urls, callback, images)
   });
 }
 
-function loadTexture1(gl, image)
+function loadTexture(gl, image)
 {
   var texture = gl.createTexture();   // Create a texture object
   if (!texture)
@@ -235,7 +176,35 @@ function draw(gl, n, texture, u_Sampler)
   // Set the texture unit 0 to the sampler
   gl.uniform1i(u_Sampler, 0);
 
-  gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
 
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); // Draw the rectangle
+}
+
+var isload = false;
+var isloading = false;
+var image;
+var texture;
+
+function update()
+{
+  if (!isload)
+  {
+    if (!isloading)
+    {
+      loadImage('../resources/sky.jpg', (img) =>
+      {
+        image = img;
+        texture = loadTexture(gl, image);
+        isload = true;
+      });
+    }
+    isloading = true;
+  } else
+  {
+    gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
+
+    draw(gl, n, texture, u_Sampler);
+  }
+
+  requestAnimationFrame(update)
 }
